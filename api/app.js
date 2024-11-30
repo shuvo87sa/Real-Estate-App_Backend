5f11037654f0ce4e69deb258ca4d5df898e00dd0
shuvo87sa <sabbirnew2023@gmail.com>
2024-11-30 22:38:08 +0600

    sucessfully send postman data to vs code ,using postman

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
 
 const app = express();
 
-app.use("/api/test", (req,res)=>{
-    res.send("It works!")
-})
+app.use(express.json());
+
+app.use("/api/posts",postRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
     console.log("Server is running!");
---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = (req,res)=>{
+    //db operations
+    console.log(req.body);
+    
+}
+export const login = (req,res)=>{
+    //db operations
+}
+export const logout = (req,res)=>{
+    //db operations
+}
