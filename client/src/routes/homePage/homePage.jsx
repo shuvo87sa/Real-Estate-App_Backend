cfc8b683f429b7d3eda1a1d3388c1b78fbe51545
shuvo87sa <sabbirnew2023@gmail.com>
2024-12-01 18:54:18 +0600

    Addig command line and git code for backend

--- client/src/routes/homePage/homePage.jsx
+++ client/src/routes/homePage/homePage.jsx
@@ -11,25 +11,24 @@ function HomePage() {
     <div className="homePage">
       <div className="textContainer">
         <div className="wrapper">
-          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <h1 className="title">Find Apartment and bachelor room,Get Your Dream Place</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            explicabo suscipit cum eius, iure est nulla animi consequatur
-            facilis id pariatur fugit quos laudantium temporibus dolor ea
-            repellat provident impedit!
+          At UrbanHaven, we help you find the ideal apartment or bachelor room with ease. 
+          Whether you're a student or a young professional, our platform offers a wide selection of 
+          affordable and quality living spaces.
           </p>
           <SearchBar />
           <div className="boxes">
             <div className="box">
-              <h1>16+</h1>
+              <h1>3+</h1>
               <h2>Years of Experience</h2>
             </div>
             <div className="box">
-              <h1>200</h1>
+              <h1>15</h1>
               <h2>Award Gained</h2>
             </div>
             <div className="box">
-              <h1>2000+</h1>
+              <h1>60+</h1>
               <h2>Property Ready</h2>
             </div>
           </div>
