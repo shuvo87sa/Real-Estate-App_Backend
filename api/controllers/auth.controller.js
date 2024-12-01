afb7a5245576b71bd3af2b79cb7d858acf03658e
shuvo87sa <sabbirnew2023@gmail.com>
2024-12-01 16:10:33 +0600

    finishes API authentication,register,login,logout

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -92,6 +92,6 @@ export const login = async (req,res)=>{
         res.status(500).json({message:"Failed to login!"})        
     }
 };
-export const logout = (req,res)=>{
-    //db operations
+export const logout = (req, res)=>{
+    res.clearCookie("token").status(200).json({ message: "Logout Sucessful" });
 };   
